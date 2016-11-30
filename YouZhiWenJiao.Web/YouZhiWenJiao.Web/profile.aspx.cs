db339873b135cf5fae417054302b06ce28e342c9
Jaffer Li <Jaffer.Li@cdc.berlitz.net>
2016-11-30 19:03:28 +0800

    profile

--- YouZhiWenJiao.Web/YouZhiWenJiao.Web/profile.aspx.cs
+++ YouZhiWenJiao.Web/YouZhiWenJiao.Web/profile.aspx.cs
@@ -10,35 +10,38 @@ using YouZhiWenJiao.Web.Common;
 
 namespace YouZhiWenJiao.Web
 {
-    public partial class profile : CommonPage
-    {
-        protected List<CommonModel> CompanyProfileList;
-        protected List<CommonTypeModel> CompanyProfileTypeList;
-        
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            CompanyProfileTypeList = new List<CommonTypeModel>();
-            var sql = "select * from type where categoryid = " + (int)category.公司简介 + " order by id";
-            
-            sqlCmd.CommandText = sql;
-            IDataReader rdr = sqlCmd.ExecuteReader();
+	public partial class profile : CommonPage
+	{
+		protected List<CommonModel> CompanyProfileList;
+		protected List<CommonTypeModel> CompanyProfileTypeList;
 
-            while (rdr.Read())
-            {
-                CompanyProfileTypeList.Add(new CommonTypeModel() { id = ToInt(rdr["id"]), categoryid = ToInt(rdr["categoryid"]), description = rdr["description"].ToString() });
-            }
-            rdr.Close();
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			CompanyProfileTypeList = new List<CommonTypeModel>();
+			var sql = "select * from type where categoryid = " + (int)category.公司简介 + " order by id";
 
-            CompanyProfileList = new List<CommonModel>();
-            sql = "select * from product where categoryid = " + (int)category.公司简介 + " order by typeid";
-            sqlCmd.CommandText = sql;
-            rdr = sqlCmd.ExecuteReader();
+			sqlCmd.CommandText = sql;
+			IDataReader rdr = sqlCmd.ExecuteReader();
 
-            while (rdr.Read())
-            {
-                CompanyProfileList.Add(new CommonModel() { id = rdr["id"].ToString(), typeid = ToInt(rdr["typeid"]), title = rdr["title"].ToString(), content = rdr["content"].ToString() });
-            }
-            rdr.Close();
-        }        
-    }
+			while(rdr.Read())
+			{
+				CompanyProfileTypeList.Add(new CommonTypeModel() { id = ToInt(rdr["id"]), categoryid = ToInt(rdr["categoryid"]), description = rdr["description"].ToString() });
+			}
+			rdr.Close();
+
+			CompanyProfileList = new List<CommonModel>();
+			foreach(var cpt in CompanyProfileTypeList)
+			{
+				sql = "select * from product where categoryid = " + (int)category.公司简介 + " and typeid = " + cpt.id + " and (deleted <> 1 or deleted is null) and showinhomepage = 1 order by datetime desc limit 0,1;";
+				sqlCmd.CommandText = sql;
+				rdr = sqlCmd.ExecuteReader();
+
+				while(rdr.Read())
+				{
+					CompanyProfileList.Add(new CommonModel() { id = rdr["id"].ToString(), typeid = ToInt(rdr["typeid"]), title = rdr["title"].ToString(), content = rdr["content"].ToString() });
+				}
+				rdr.Close();
+			}
+		}
+	}
 }
